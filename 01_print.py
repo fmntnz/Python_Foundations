69e092324fe16214128ea087bf470321d1aea825
fmntnz <C0ndeAstar0th@gmail.com>
2023-01-31 15:08:23 -0500

    Python Foundations 1

---
+++ 01_print.py
@@ -0,0 +1,22 @@
+print("Hi This is the file Number 01")
+
+print("Hi I'm Freddy and I'm 31 yo")
+
+# operaciones...
+
+print(12 + 5)
+print(10 - 5)
+print(2 * 3)
+print(8 / 2)
+
+# Esto es un comentario
+"""
+varias
+lineas
+1
+"""
+'''
+varias 
+lineas 
+2
+'''
